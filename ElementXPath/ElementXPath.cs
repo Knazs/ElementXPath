d7d016a06f3248425eada46bcbe0662af781faa8
Nikita Zalesskiy <nikitazalesskiy.social@gmail.com>
2022-08-15 11:05:02 +0300

    Add project files.

---
+++ ElementXPath/ElementXPath.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace Knazs.Xml.XPath;
+
+public sealed class XElementNames : IEnumerable<string>
+{
+    private XElementNames(ArraySegment<string> elementNames)
+    {
+        _elementNames = elementNames;
+    }
+
+    private readonly ArraySegment<string> _elementNames;
+
+    public static XElementNames Parse(string xpath)
+    {
+        if (!TryParse(xpath, out var xelementNames))
+        {
+            throw new FormatException(nameof(xpath));
+        }
+
+        return xelementNames;
+    }
+
+    public static bool TryParse(string xabsolutePath, [NotNullWhen(true)] out XElementNames? xelementNames)
+    {
+        if (string.IsNullOrEmpty(xabsolutePath))
+        {
+            goto FalseCase;
+        }
+
+        var xsubPaths = xabsolutePath.Split('/');
+
+        string xsubPath;
+        var xsubPathsCount = xsubPaths.Length;
+
+        var index = Convert.ToInt32(string.IsNullOrEmpty(xsubPaths[0]));
+
+        for (; index < xsubPathsCount; ++index)
+        {
+            xsubPath = xsubPaths[index];
+
+            if (!XmlReader.IsName(xsubPath))
+            {
+                goto FalseCase;
+            }
+        }
+
+        ArraySegment<string> elementNames = new(xsubPaths, 1, xsubPathsCount - 1);
+
+        xelementNames = new(elementNames);
+
+        return true;
+
+    FalseCase:
+
+        xelementNames = default;
+
+        return false;
+    }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        return _elementNames.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _elementNames.GetEnumerator();
+    }
+}
---
+++ ElementXPath/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knazs.Xml.XPath
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var @return = XElementNames.TryParse("asds:ASD:something", out var xElementNames);
+            
+            @return = XElementNames.TryParse("/", out xElementNames);
+
+            @return = XElementNames.TryParse("/something", out xElementNames);
+        }
+    }
+}
